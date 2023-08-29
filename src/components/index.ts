a5eb6093560ebfb7830a1d63b18331cc6d52282a
Loa212 <anobileloris@gmail.com>
2023-08-29 11:28:39 +0200

    add base components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Button from "./Button.astro";
+import Hero from "./Hero.astro";
+import Navbar from "./Navbar.astro";
+import ThemeIcon from "./ThemeIcon.astro";
+import Drawer from "./Drawer.astro";
+
+export { Button, Hero, Navbar, ThemeIcon, Drawer };
