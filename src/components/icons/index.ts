a0f0ecaba0eca8c4a77915e47c2c58edb3f412e4
Loa212 <anobileloris@gmail.com>
2023-09-01 10:04:11 +0200

    language switch

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,8 @@
 import ChevronDown from "./chevron-down.astro";
+import Languages from "./languages.astro";
 
 export {
   ChevronDown,
+  Languages,
   // ... other icons
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Hero from "./Hero.astro";
 import Navbar from "./Navbar.astro";
 import ThemeIcon from "./ThemeIcon.astro";
 import Drawer from "./Drawer.astro";
+import LanguageSwitch from "./LanguageSwitch.astro";
 
-export { Button, Hero, Navbar, ThemeIcon, Drawer };
+export { Button, Hero, Navbar, ThemeIcon, Drawer, LanguageSwitch };
