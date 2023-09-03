40816802cfce9b54dfd8f0bd37d03940191564f9
Loa212 <anobileloris@gmail.com>
2023-09-03 22:50:54 +0200

    rm   output: "hybrid"

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
   integrations: [tailwind(), mdx()],
-  adapter: netlify()
+  adapter: netlify(),
 });
