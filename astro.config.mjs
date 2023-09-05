07eed9e5e6a2fa6df33862326e8c975dac00641f
Loa212 <anobileloris@gmail.com>
2023-09-05 16:33:52 +0200

    fix errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,76 +4,12 @@ import mdx from "@astrojs/mdx";
 import NetlifyCMS from "astro-netlify-cms";
 
 import netlify from "@astrojs/netlify/functions";
+import { config } from "./src/plugins/netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "hybrid",
-  integrations: [
-    tailwind(),
-    mdx(),
-    NetlifyCMS({
-      previewStyles: [
-        "https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=IBM+Plex+Sans:wght@400;700&display=swap",
-        "/src/styles/previews.css",
-      ],
-      config: {
-        backend: {
-          name: "git-gateway",
-          branch: "main",
-        },
-        media_folder: "src/assets",
-        public_folder: "/assets",
-        collections: [
-          {
-            name: "blog",
-            label: "Blog Posts",
-            folder: "src/content/blog",
-            create: true,
-            delete: true,
-            slug: "{{slug}}",
-            fields: [{ label: "title", name: "title", widget: "string" }],
-          },
-          {
-            name: "services",
-            label: "Services",
-            description: "this is a test description for services",
-            folder: "src/content/services",
-            create: true,
-            delete: true,
-            slug: "{{slug}}",
-            fields: [
-              { label: "title", name: "title", widget: "string" },
-              { label: "subtitle", name: "subtitle", widget: "string" },
-              { label: "icon", name: "icon", widget: "image" },
-            ],
-          },
-          {
-            name: "portfolio",
-            label: "Portfolio",
-            description: "this is a test description for portfolio",
-            folder: "src/content/portfolio",
-            create: true,
-            delete: true,
-            slug: "{{slug}}",
-            fields: [
-              { label: "title", name: "title", widget: "string" },
-              { label: "description", name: "description", widget: "string" },
-              { label: "date", name: "date", widget: "datetime" },
-              { label: "url", name: "url", widget: "string" },
-              { label: "hero", name: "hero", widget: "image" },
-              {
-                label: "images",
-                name: "images",
-                widget: "list",
-                fields: [{ label: "image", name: "image", widget: "image" }],
-              },
-              { label: "Content", name: "body", widget: "markdown" },
-            ],
-          },
-        ],
-      },
-    }),
-  ],
+  integrations: [tailwind(), mdx(), NetlifyCMS({ config })],
   adapter: netlify(),
 });
---
+++ src/plugins/netlify-cms.ts
@@ -0,0 +1,58 @@
+import type { CmsConfig } from "netlify-cms-core";
+export const config: Omit<CmsConfig, "load_config_file" | "local_backend"> = {
+  backend: {
+    name: "git-gateway",
+    branch: "main",
+  },
+  publish_mode: "editorial_workflow",
+  media_folder: "src/assets",
+  public_folder: "/assets",
+  collections: [
+    {
+      name: "blog",
+      label: "Blog Posts",
+      folder: "src/content/blog",
+      create: true,
+      delete: true,
+      slug: "{{slug}}",
+      fields: [{ label: "title", name: "title", widget: "string" }],
+    },
+    {
+      name: "services",
+      label: "Services",
+      description: "this is a test description for services",
+      folder: "src/content/services",
+      create: true,
+      delete: true,
+      slug: "{{slug}}",
+      fields: [
+        { label: "title", name: "title", widget: "string" },
+        { label: "subtitle", name: "subtitle", widget: "string" },
+        { label: "icon", name: "icon", widget: "image" },
+      ],
+    },
+    {
+      name: "portfolio",
+      label: "Portfolio",
+      description: "this is a test description for portfolio",
+      folder: "src/content/portfolio",
+      create: true,
+      delete: true,
+      slug: "{{slug}}",
+      fields: [
+        { label: "title", name: "title", widget: "string" },
+        { label: "description", name: "description", widget: "string" },
+        { label: "date", name: "date", widget: "datetime" },
+        { label: "url", name: "url", widget: "string" },
+        { label: "hero", name: "hero", widget: "image" },
+        {
+          label: "images",
+          name: "images",
+          widget: "list",
+          fields: [{ label: "image", name: "image", widget: "image" }],
+        },
+        { label: "Content", name: "body", widget: "markdown" },
+      ],
+    },
+  ],
+};
