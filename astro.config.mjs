33bfce1cb75544a53edd961b5aeedf2c436aa64b
Loa212 <anobileloris@gmail.com>
2023-09-05 16:55:22 +0200

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
   integrations: [tailwind(), mdx()],
-  adapter: netlify(),
 });
