381e0f6f04e26df7c321e169887dbae86ba73c06
Loa212 <anobileloris@gmail.com>
2023-09-18 16:56:42 +0200

    improve cookiebanner.ts

--- src/utils/cookiebanner.ts
+++ src/utils/cookiebanner.ts
@@ -1,195 +1,306 @@
-// @ts-nocheck
-// Check if the user has previously saved cookie preferences
-const savedCookiePrefs = localStorage.getItem("cookiePrefs");
-let acceptedStrictlyNecessary = false;
-
-// Function to show the cookie preferences modal
-function showCookiePrefsModal() {
-  //before opening we check if the user has already saved preferences
-  // and we apply them to the checkboxes
-  checkSavedCookiePrefs();
-
-  const modal = document.getElementById("cookiePrefsModal");
-  modal.showModal();
-}
-
-// Function to hide the cookie preferences modal
-function hideCookiePrefsModal() {
-  const modal = document.getElementById("cookiePrefsModal");
-  modal.close();
-}
-
-// Function to save cookie preferences in localStorage
-function saveCookiePreferences(preferences) {
-  localStorage.setItem("cookiePrefs", JSON.stringify(preferences));
-
-  if (!preferences.strictlyNecessary) {
-    // Disable scripts if strictly necessary cookies are not accepted
-    console.log(
-      "Scripts are disabled because strictly necessary cookies are not accepted.",
-    );
-    window.location.reload();
-  }
-
-  // Hide the main cookie banner
-  const mainCookieBanner = document.getElementById("cookieBanner");
-  mainCookieBanner.style.display = "none";
-
-  // Show the smaller banner at the bottom left corner if necessary cookies are accepted
-  const smallerBanner = document.getElementById("smallerCookieBanner");
-  if (preferences.strictlyNecessary) {
-    smallerBanner.style.display = "block";
-  }
-}
-
-// Function to accept all cookies
-function acceptAllCookies() {
-  const preferences = {
-    strictlyNecessary: true,
-    preferences: true,
-    thirdParty: true,
-  };
-
-  acceptedStrictlyNecessary = true;
-  saveCookiePreferences(preferences);
-  savePrefsInModal(preferences);
-  hideCookiePrefsModal();
-}
-
-// Event listener for "Cookie Preferences" link
-const cookiePrefsLink = document.getElementById("cookiePrefsLink");
-if (cookiePrefsLink) {
-  cookiePrefsLink.addEventListener("click", (e) => {
-    e.preventDefault();
-    showCookiePrefsModal();
-  });
-}
-
-// Event listener for "Cookie Preferences" link 2
-const cookiePrefsLink2 = document.getElementById("cookiePrefsLink2");
-if (cookiePrefsLink2) {
-  cookiePrefsLink2.addEventListener("click", (e) => {
-    e.preventDefault();
-    showCookiePrefsModal();
-  });
-}
-
-// Event listener for Close button in modal
-const closeCookiePrefsModal = document.getElementById("closeCookiePrefsModal");
-if (closeCookiePrefsModal) {
-  closeCookiePrefsModal.addEventListener("click", () => {
-    hideCookiePrefsModal();
-  });
-}
-
-// Event listener for "Accept All" button
-const acceptAllButton = document.getElementById("acceptAllButton");
-if (acceptAllButton) {
-  acceptAllButton.addEventListener("click", () => {
-    acceptAllCookies();
-  });
-}
-
-// Event listener for "Save Preferences" button
-const saveCookiePrefsButton = document.getElementById("saveCookiePrefs");
-if (saveCookiePrefsButton) {
-  saveCookiePrefsButton.addEventListener("click", () => {
-    // Get user preferences from the modal
-    const preferences = {
-      strictlyNecessary: document.getElementById("strictlyNecessaryCheckbox")
-        .checked,
-      preferences: document.getElementById("preferencesCheckbox").checked,
-      thirdParty: document.getElementById("thirdPartyCheckbox").checked,
-    };
-
-    if (!preferences.strictlyNecessary) {
-      // Disable scripts if strictly necessary cookies are not accepted
-      console.log(
-        "Scripts are disabled because strictly necessary cookies are not accepted.",
-      );
-    }
-
-    // Save preferences and close modal
-    saveCookiePreferences(preferences);
-    hideCookiePrefsModal();
-  });
-}
-
-// event listener for "strictlyNecessaryCheckbox"
-// when the checkbox gets unchecked, the other checkboxes get unchecked too
-const strictlyNecessaryCheckbox = document.getElementById(
-  "strictlyNecessaryCheckbox",
-);
-strictlyNecessaryCheckbox.addEventListener("change", () => {
-  if (!strictlyNecessaryCheckbox.checked) {
-    document.getElementById("preferencesCheckbox").checked = false;
-    document.getElementById("thirdPartyCheckbox").checked = false;
-  }
-});
-
-const savePrefsInModal = (prefs) => {
-  document.getElementById("strictlyNecessaryCheckbox").checked =
-    prefs.strictlyNecessary;
-  document.getElementById("preferencesCheckbox").checked = prefs.preferences;
-  document.getElementById("thirdPartyCheckbox").checked = prefs.thirdParty;
+export type CookiePreferences = {
+  strictlyNecessary: boolean;
+  preferences: boolean;
+  thirdParty: boolean;
 };
 
-const checkSavedCookiePrefs = () => {
+const panic = () => window.location.reload();
+
+const showMainCookieBanner = () => {
+  const mainCookieBanner =
+    document.querySelector<HTMLDivElement>("#cookieBanner");
+  if (mainCookieBanner) {
+    mainCookieBanner.style.display = "block";
+  } else {
+    panic(); // reload the page
+  }
+};
+
+const showSmallerCookieBanner = () => {
+  const smallerBanner = document.querySelector<HTMLDivElement>(
+    "#smallerCookieBanner"
+  );
+  if (smallerBanner) {
+    smallerBanner.style.display = "block";
+  } else {
+    panic(); // reload the page
+  }
+};
+
+const hideMainCookieBanner = () => {
+  const mainCookieBanner =
+    document.querySelector<HTMLDivElement>("#cookieBanner");
+  if (mainCookieBanner) {
+    mainCookieBanner.style.display = "none";
+  } else {
+    panic(); // reload the page
+  }
+};
+
+const hideSmallerCookieBanner = () => {
+  const smallerBanner = document.querySelector<HTMLDivElement>(
+    "#smallerCookieBanner"
+  );
+  if (smallerBanner) {
+    smallerBanner.style.display = "none";
+  } else {
+    panic(); // reload the page
+  }
+};
+
+const showCookiePrefsModal = () => {
+  const modal = document.querySelector<HTMLDialogElement>("#cookiePrefsModal");
+  modal?.showModal();
+};
+
+const hideCookiePrefsModal = () => {
+  const modal = document.querySelector<HTMLDialogElement>("#cookiePrefsModal");
+  modal?.close();
+};
+
+function mountCookiePolicyBanner(timeout: number = 1000) {
+  setTimeout(() => {
+    main();
+  }, timeout);
+}
+
+function main() {
+  // Check if the user has previously saved cookie preferences
   const savedCookiePrefs = localStorage.getItem("cookiePrefs");
+  let acceptedStrictlyNecessary = false;
+
+  // Check if the user has previously saved preferences and apply them
   if (savedCookiePrefs) {
-    const preferences = JSON.parse(savedCookiePrefs);
+    const preferences: CookiePreferences = JSON.parse(savedCookiePrefs);
     // Apply saved preferences here (customize this part)
-    document.getElementById("strictlyNecessaryCheckbox").checked =
-      preferences.strictlyNecessary;
-    document.getElementById("preferencesCheckbox").checked =
-      preferences.preferences;
-    document.getElementById("thirdPartyCheckbox").checked =
-      preferences.thirdParty;
+    const strictlyNecessaryCheckbox = document.querySelector<HTMLInputElement>(
+      "#strictlyNecessaryCheckbox"
+    );
+    const preferencesCheckbox = document.querySelector<HTMLInputElement>(
+      "#preferencesCheckbox"
+    );
+    const thirdPartyCheckbox = document.querySelector<HTMLInputElement>(
+      "#thirdPartyCheckbox"
+    );
+    if (
+      strictlyNecessaryCheckbox &&
+      preferencesCheckbox &&
+      thirdPartyCheckbox
+    ) {
+      strictlyNecessaryCheckbox.checked = preferences.strictlyNecessary;
+      preferencesCheckbox.checked = preferences.preferences;
+      thirdPartyCheckbox.checked = preferences.thirdParty;
+    } else {
+      panic(); // reload the page
+    }
 
     acceptedStrictlyNecessary = preferences.strictlyNecessary;
 
     if (!acceptedStrictlyNecessary) {
       // Disable scripts if strictly necessary cookies are not accepted
       console.log(
-        "Scripts are disabled because strictly necessary cookies are not accepted.",
+        "Scripts are disabled because strictly necessary cookies are not accepted."
       );
+      // show the main cookie banner
+      showMainCookieBanner();
     } else {
       // Hide the main cookie banner
-      const mainCookieBanner = document.getElementById("cookieBanner");
-      mainCookieBanner.style.display = "none";
+      hideMainCookieBanner();
 
       // Show the smaller banner at the bottom left corner
-      const smallerBanner = document.getElementById("smallerCookieBanner");
-      smallerBanner.style.display = "block";
+      showSmallerCookieBanner();
     }
   }
-};
 
-// Check if the user has previously saved preferences and apply them
-if (savedCookiePrefs) {
-  const preferences = JSON.parse(savedCookiePrefs);
-  // Apply saved preferences here (customize this part)
-  document.getElementById("strictlyNecessaryCheckbox").checked =
-    preferences.strictlyNecessary;
-  document.getElementById("preferencesCheckbox").checked =
-    preferences.preferences;
-  document.getElementById("thirdPartyCheckbox").checked =
-    preferences.thirdParty;
+  // ------------------ Event listeners & helpers ------------------
 
-  acceptedStrictlyNecessary = preferences.strictlyNecessary;
+  // Function to save cookie preferences in localStorage
+  function saveCookiePreferences(preferences: CookiePreferences) {
+    localStorage.setItem("cookiePrefs", JSON.stringify(preferences));
 
-  if (!acceptedStrictlyNecessary) {
-    // Disable scripts if strictly necessary cookies are not accepted
-    console.log(
-      "Scripts are disabled because strictly necessary cookies are not accepted.",
-    );
-  } else {
+    if (!preferences.strictlyNecessary) {
+      // Disable scripts if strictly necessary cookies are not accepted
+      console.log(
+        "Scripts are disabled because strictly necessary cookies are not accepted."
+      );
+      panic(); // reload the page
+    }
+
     // Hide the main cookie banner
-    const mainCookieBanner = document.getElementById("cookieBanner");
-    mainCookieBanner.style.display = "none";
+    hideMainCookieBanner();
 
-    // Show the smaller banner at the bottom left corner
-    const smallerBanner = document.getElementById("smallerCookieBanner");
-    smallerBanner.style.display = "block";
+    // Show the smaller banner at the bottom left corner if necessary cookies are accepted
+    showSmallerCookieBanner();
   }
+
+  // Function to accept all cookies
+  function acceptAllCookies() {
+    const preferences: CookiePreferences = {
+      strictlyNecessary: true,
+      preferences: true,
+      thirdParty: true,
+    };
+
+    acceptedStrictlyNecessary = true;
+    saveCookiePreferences(preferences);
+    savePrefsInModal(preferences);
+    hideCookiePrefsModal();
+  }
+
+  // Event listener for "Cookie Preferences" link
+  const cookiePrefsLink =
+    document.querySelectorAll<HTMLButtonElement>("#cookiePrefsLink");
+  cookiePrefsLink.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      showCookiePrefsModal();
+    });
+  });
+
+  // Event listener for Close button in modal
+  const closeCookiePrefsModal = document.querySelector<HTMLButtonElement>(
+    "#closeCookiePrefsModal"
+  );
+  if (closeCookiePrefsModal) {
+    closeCookiePrefsModal.addEventListener("click", () => {
+      hideCookiePrefsModal();
+    });
+  } else {
+    panic(); // reload the page
+  }
+
+  // Event listener for "Accept All" button
+  const acceptAllButton =
+    document.querySelector<HTMLButtonElement>("#acceptAllButton");
+  if (acceptAllButton) {
+    acceptAllButton.addEventListener("click", () => {
+      acceptAllCookies();
+    });
+  } else {
+    panic(); // reload the page
+  }
+
+  // Event listener for "Save Preferences" button
+  const saveCookiePrefsButton =
+    document.querySelector<HTMLButtonElement>("#saveCookiePrefs");
+  if (saveCookiePrefsButton) {
+    saveCookiePrefsButton.addEventListener("click", () => {
+      // Get user preferences from the modal
+      const strictlyNecessaryCheckbox =
+        document.querySelector<HTMLInputElement>("#strictlyNecessaryCheckbox");
+      const preferencesCheckbox = document.querySelector<HTMLInputElement>(
+        "#preferencesCheckbox"
+      );
+      const thirdPartyCheckbox = document.querySelector<HTMLInputElement>(
+        "#thirdPartyCheckbox"
+      );
+      const preferences: CookiePreferences = {
+        strictlyNecessary: strictlyNecessaryCheckbox?.checked || false,
+        preferences: preferencesCheckbox?.checked || false,
+        thirdParty: thirdPartyCheckbox?.checked || false,
+      };
+
+      if (!preferences.strictlyNecessary) {
+        // Disable scripts if strictly necessary cookies are not accepted
+        console.log(
+          "Scripts are disabled because strictly necessary cookies are not accepted."
+        );
+      }
+
+      // Save preferences and close modal
+      saveCookiePreferences(preferences);
+      hideCookiePrefsModal();
+    });
+  } else {
+    panic(); // reload the page
+  }
+
+  // Event listener for "strictlyNecessaryCheckbox"
+  // when the checkbox gets unchecked, the other checkboxes get unchecked too
+  const strictlyNecessaryCheckbox = document.querySelector<HTMLInputElement>(
+    "#strictlyNecessaryCheckbox"
+  );
+  if (strictlyNecessaryCheckbox) {
+    strictlyNecessaryCheckbox.addEventListener("change", () => {
+      if (!strictlyNecessaryCheckbox.checked) {
+        const preferencesCheckbox = document.querySelector<HTMLInputElement>(
+          "#preferencesCheckbox"
+        );
+        const thirdPartyCheckbox = document.querySelector<HTMLInputElement>(
+          "#thirdPartyCheckbox"
+        );
+        if (preferencesCheckbox && thirdPartyCheckbox) {
+          preferencesCheckbox.checked = false;
+          thirdPartyCheckbox.checked = false;
+        }
+      }
+    });
+  }
+
+  const savePrefsInModal = (preferences: CookiePreferences) => {
+    const strictlyNecessaryCheckbox = document.querySelector<HTMLInputElement>(
+      "#strictlyNecessaryCheckbox"
+    );
+    const preferencesCheckbox = document.querySelector<HTMLInputElement>(
+      "#preferencesCheckbox"
+    );
+    const thirdPartyCheckbox = document.querySelector<HTMLInputElement>(
+      "#thirdPartyCheckbox"
+    );
+    if (
+      strictlyNecessaryCheckbox &&
+      preferencesCheckbox &&
+      thirdPartyCheckbox
+    ) {
+      strictlyNecessaryCheckbox.checked = preferences.strictlyNecessary;
+      preferencesCheckbox.checked = preferences.preferences;
+      thirdPartyCheckbox.checked = preferences.thirdParty;
+    } else {
+      panic(); // reload the page
+    }
+  };
+
+  const checkSavedCookiePrefs = () => {
+    const savedCookiePrefs = localStorage.getItem("cookiePrefs");
+    if (savedCookiePrefs) {
+      const preferences: CookiePreferences = JSON.parse(savedCookiePrefs);
+      // Apply saved preferences here (customize this part)
+      const strictlyNecessaryCheckbox =
+        document.querySelector<HTMLInputElement>("#strictlyNecessaryCheckbox");
+      const preferencesCheckbox = document.querySelector<HTMLInputElement>(
+        "#preferencesCheckbox"
+      );
+      const thirdPartyCheckbox = document.querySelector<HTMLInputElement>(
+        "#thirdPartyCheckbox"
+      );
+      if (
+        strictlyNecessaryCheckbox &&
+        preferencesCheckbox &&
+        thirdPartyCheckbox
+      ) {
+        strictlyNecessaryCheckbox.checked = preferences.strictlyNecessary;
+        preferencesCheckbox.checked = preferences.preferences;
+        thirdPartyCheckbox.checked = preferences.thirdParty;
+      } else {
+        panic(); // reload the page
+      }
+
+      acceptedStrictlyNecessary = preferences.strictlyNecessary;
+
+      if (!acceptedStrictlyNecessary) {
+        // Disable scripts if strictly necessary cookies are not accepted
+        console.log(
+          "Scripts are disabled because strictly necessary cookies are not accepted."
+        );
+      } else {
+        // Hide the main cookie banner
+        hideMainCookieBanner();
+
+        // Show the smaller banner at the bottom left corner
+        showSmallerCookieBanner();
+      }
+    }
+  };
 }
+
+export { mountCookiePolicyBanner };
