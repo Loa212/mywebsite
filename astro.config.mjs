bd8067fc550e0e2ea9c64dde7791c92bd1f3a73a
Loa212 <anobileloris@gmail.com>
2023-11-04 17:07:44 +0100

    add vercel integration for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   integrations: [tailwind(), mdx()],
+  adapter: vercel()
 });
