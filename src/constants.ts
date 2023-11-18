6d417a7f2a5c3190cfde64ee95138f2d3ea6c584
Loa212 <anobileloris@gmail.com>
2023-11-18 16:15:26 +0100

    privacy+terms

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 const SITE_TITLE = "Loa212";
 const SITE_DESCRIPTION =
   "Tailor-made web development services for your business.";
+const CONTACT_EMAIL = "hello@loa212.com";
 
-export { SITE_TITLE, SITE_DESCRIPTION };
+export { SITE_TITLE, SITE_DESCRIPTION, CONTACT_EMAIL };
