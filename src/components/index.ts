a25562b29e63cafb82e92e4f9f138c848d6c9c5f
Loa212 <anobileloris@gmail.com>
2023-11-27 18:49:51 +0100

    first commit

--- src/components/index.ts
+++ src/components/index.ts
@@ -12,10 +12,10 @@ import Testimonials from "./Testimonials.astro";
 import Pricing from "./Pricing.astro";
 import BottomCta from "./BottomCta.astro";
 import Footer from "./Footer.astro";
-// @ts-expect-error
 import CookiesBanner from "./CookiesBanner.astro";
 import ScrollTop from "./ScrollTop.astro";
 import Contact from "./Contact.astro";
+import PricelessPricing from "./PricelessPricing.astro";
 
 export {
   Button,
@@ -35,5 +35,6 @@ export {
   CookiesBanner,
   ScrollTop,
   Contact,
+  PricelessPricing,
   //more...
 };
